d0f2bfe8342246a0cbc3eb042e0a3e5a76467a1a
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-10-18 15:02:32 -0300

    Fragua - Setup file
    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fragua",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'pytest',
+        'typing_extensions',
+    ],
+)
