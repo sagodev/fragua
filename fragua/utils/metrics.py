88fe71c341fbe74fe803fae6864c237c64a2db32
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-10-25 19:12:15 -0300

    Fix Linting
    add use for TYPE_CHECKING.

--- fragua/utils/metrics.py
+++ fragua/utils/metrics.py
@@ -10,9 +10,12 @@ by serializing the data in a consistent way.
 import hashlib
 import json
 from datetime import datetime, timezone
-from typing import Any, cast, Optional, Dict
+from typing import Any, cast, Optional, Dict, TYPE_CHECKING
 import pandas as pd
 from fragua.utils.logger import get_logger
+
+if TYPE_CHECKING:
+    from fragua.store import Wagon, Box, Container
 
 logger = get_logger(__name__)
 
@@ -67,11 +70,10 @@ def get_local_time_and_offset() -> tuple[str, str]:
 
 def determine_storage_type(storage: Any) -> str | None:
     """Extract a meaningful output type for the operation metadata."""
-    from fragua.store import (
-        Wagon,
-        Box,
-        Container,
-    )  # pylint: disable=import-outside-toplevel
+    if TYPE_CHECKING:
+        Wagon: type
+        Box: type
+        Container: type
 
     if isinstance(storage, Wagon):
         return "wagon"
