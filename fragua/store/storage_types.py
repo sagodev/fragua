15cd3e8f63582b11f8a52d9f35c7127c72420cd0
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-10-29 00:49:26 -0300

    Update storage_types.py
    Refactor container storage type.

--- fragua/store/storage_types.py
+++ fragua/store/storage_types.py
@@ -1,21 +1,53 @@
 """
-Storage types for storing different types of data.
+Storage types for storing different kinds of data.
 """
 
-from fragua.store.storage import register_storage, T
-from fragua.store.storage import Storage
+from typing import Any, Dict, Union
+from fragua.store.storage import register_storage, Storage
 
 
 @register_storage
-class Wagon(Storage[T]):
-    """Storage type for storing raw data."""
+class Wagon(Storage[Any]):
+    """Storage type for raw data."""
 
 
 @register_storage
-class Box(Storage[T]):
-    """Storage type for storing forged data."""
+class Box(Storage[Any]):
+    """Storage type for transformed data."""
 
 
 @register_storage
-class Container(Storage[T]):
-    """Storage type for storing delivery data."""
+class Container(Storage[Any]):
+    """
+    Storage designed to contain other storages (Wagon and Box).
+    """
+
+    def __init__(self, name: str = "container"):
+        super().__init__(data=None)
+        self.name = name
+        self._content: Dict[str, Union[Wagon, Box]] = {}
+
+    def add_storage(self, storage: Union[Wagon, Box]) -> None:
+        """Add a sub-storage (Wagon or Box) to the container."""
+        storage_name = getattr(storage, "name", storage.__class__.__name__)
+        self._content[storage_name] = storage
+
+    def get_storage(self, name: str) -> Union[Wagon, Box]:
+        """Retrieve a specific sub-storage by name."""
+        return self._content[name]
+
+    def list_storages(self) -> dict[str, str]:
+        """Return a mapping of contained storages: name -> type."""
+        return {name: s.__class__.__name__ for name, s in self._content.items()}
+
+    def remove_storage(self, name: str) -> None:
+        """Remove a sub-storage by name."""
+        self._content.pop(name, None)
+
+    def clear(self) -> None:
+        """Remove all contained storages."""
+        self._content.clear()
+
+    def __repr__(self) -> str:
+        items = ", ".join(self._content.keys()) or "empty"
+        return f"<Container: {items}>"
