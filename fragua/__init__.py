8810351f214ab011b69562e232c34363f594a50f
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-11-01 20:04:32 -0300

    Update __init__.py

--- fragua/__init__.py
+++ fragua/__init__.py
@@ -1,173 +1,19 @@
-"""Fragua package."""
+"""Fragua Module."""
 
 # ------------------- Agents ------------------- #
-from fragua.agents import Agent, StoreManager, Miner, Blacksmith, Haulier
+from fragua.agents import *
 
+# ------------------- Functions ------------------- #
+from fragua.functions import *
 
 # ------------------- Style ------------------- #
-from fragua.style import (
-    Style,
-    # Forge Styles
-    ForgeStyle,
-    ReportForgeStyle,
-    MLForgeStyle,
-    AnalysisForgeStyle,
-    # Delivery Styles
-    DeliveryStyle,
-    SQLDeliveryStyle,
-    APIDeliveryStyle,
-    ExcelDeliveryStyle,
-    # Mine Styles
-    MineStyle,
-    CSVMineStyle,
-    ExcelMineStyle,
-    SQLMineStyle,
-    APIMineStyle,
-    # Styles Registry Functions
-    register_style,
-    get_style,
-    list_styles,
-)
+from fragua.style import *
 
 # ------------------- Store ------------------- #
-from fragua.store import (
-    Storage,
-    Store,
-    # Storage Types
-    Wagon,
-    Box,
-    Container,
-    # Storage Registry Functions
-    register_storage,
-    get_storage,
-    list_storages,
-)
-
+from fragua.store import *
 
 # ------------------- Params ------------------- #
-from fragua.params import (
-    Params,
-    # Params Registry Functions
-    register_params,
-    get_params,
-    list_params,
-    create_params_class,
-    # Mine Params
-    MineParams,
-    MineParamsT,
-    ExcelMineParams,
-    ExcelMineParamsT,
-    CSVMineParams,
-    CSVMineParamsT,
-    SQLMineParams,
-    SQLMineParamsT,
-    APIMineParams,
-    APIMineParamsT,
-    # Forge Params
-    ForgeParams,
-    ForgeParamsT,
-    MLForgeParams,
-    MLForgeParamsT,
-    ReportForgeParams,
-    ReportForgeParamsT,
-    AnalysisForgeParams,
-    AnalysisForgeParamsT,
-    # Delivery Params
-    DeliveryParams,
-    DeliveryParamsT,
-    SQLDeliveryParams,
-    SQLDeliveryParamsT,
-    APIDeliveryParams,
-    APIDeliveryParamsT,
-    ExcelDeliveryParams,
-    ExcelDeliveryParamsT,
-)
+from fragua.params import *
 
 # ------------------- Utilities ------------------- #
-from fragua.utils import (
-    get_logger,
-    Config,
-    calculate_checksum,
-    get_local_time_and_offset,
-    generate_metadata,
-    add_metadata_to_storage,
-)
-
-# ------------------- __all__ ------------------- #
-__all__ = [
-    # Base Classes
-    "Agent",
-    "Miner",
-    "Blacksmith",
-    "Haulier",
-    "StoreManager",
-    "Style",
-    "Params",
-    "Storage",
-    "Store",
-    # Storage Classes
-    "Wagon",
-    "Box",
-    "Container",
-    # Styles
-    "ForgeStyle",
-    "ReportForgeStyle",
-    "MLForgeStyle",
-    "AnalysisForgeStyle",
-    "DeliveryStyle",
-    "SQLDeliveryStyle",
-    "APIDeliveryStyle",
-    "ExcelDeliveryStyle",
-    "MineStyle",
-    "CSVMineStyle",
-    "ExcelMineStyle",
-    "SQLMineStyle",
-    "APIMineStyle",
-    # Params
-    "ForgeParams",
-    "ForgeParamsT",
-    "DeliveryParams",
-    "DeliveryParamsT",
-    "MineParams",
-    "MineParamsT",
-    "ExcelMineParams",
-    "ExcelMineParamsT",
-    "CSVMineParams",
-    "CSVMineParamsT",
-    "SQLMineParams",
-    "SQLMineParamsT",
-    "APIMineParams",
-    "APIMineParamsT",
-    "MLForgeParams",
-    "MLForgeParamsT",
-    "ReportForgeParams",
-    "ReportForgeParamsT",
-    "AnalysisForgeParams",
-    "AnalysisForgeParamsT",
-    "SQLDeliveryParams",
-    "SQLDeliveryParamsT",
-    "APIDeliveryParams",
-    "APIDeliveryParamsT",
-    "ExcelDeliveryParams",
-    "ExcelDeliveryParamsT",
-    # Params Registery Functions
-    "register_params",
-    "get_params",
-    "list_params",
-    "create_params_class",
-    # Storage Registery Functions
-    "register_storage",
-    "get_storage",
-    "list_storages",
-    # Style Registery Functions
-    "register_style",
-    "get_style",
-    "list_styles",
-    # Utilities
-    "get_logger",
-    "Config",
-    "calculate_checksum",
-    "get_local_time_and_offset",
-    "generate_metadata",
-    "add_metadata_to_storage",
-]
+from fragua.utils import *
