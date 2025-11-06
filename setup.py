73e42c98f7673c12f1d815c8d5b910ad297773f3
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-11-06 17:24:59 -0300

    Move py.typed file to fragua folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fragua",
-    version="0.1.2",
+    version="0.1.3",
     author="SagoDev",
     description="Package for creating ETL environments for data analysis",
     packages=find_packages(where=".", include=["fragua", "fragua.*"]),
