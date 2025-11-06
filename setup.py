67d343669a4dae31f85dea4104e8687281d65a5b
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-11-06 17:06:07 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="fragua",
-    version="0.1.0",
+    version="0.1.11",
     author="SagoDev",
     description="Package for creating ETL environments for data analysis",
     packages=find_packages(where=".", include=["fragua", "fragua.*"]),
+    include_package_data=True,
+    package_data={"fragua": ["py.typed"]},
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     python_requires=">=3.10",
