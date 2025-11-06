1c4b4e4126996b4dc16cdea680218aa2b3a7580a
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-11-05 22:14:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="fragua",
-    version="0.1",
-    packages=find_packages(),
+    version="0.1.0",
+    author="SagoDev",
+    description="Fragua ETL Framework",
+    packages=find_packages(where=".", include=["fragua", "fragua.*"]),
+    python_requires=">=3.10",
     install_requires=[
         "numpy",
         "pandas",
