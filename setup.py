7e2659c6f60dc1b45beb7586f80e8945f324be55
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-11-06 15:22:16 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ setup(
     name="fragua",
     version="0.1.0",
     author="SagoDev",
-    description="Fragua ETL Framework",
+    description="Package for creating ETL environments for data analysis",
     packages=find_packages(where=".", include=["fragua", "fragua.*"]),
+    long_description=open("ReadMe.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     python_requires=">=3.10",
+    url="https://github.com/SagoDev/Fragua",
     install_requires=[
         "numpy",
         "pandas",
