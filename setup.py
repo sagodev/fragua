bb5a014e1696a75954dabb7da1cb67a02b56d9be
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-11-11 20:01:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="fragua",
     version="0.2.0",
@@ -10,7 +13,7 @@ setup(
     packages=find_packages(where=".", include=["fragua", "fragua.*"]),
     include_package_data=True,
     package_data={"fragua": ["py.typed"]},
-    long_description=open("README.md", encoding="utf-8").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.10",
     url="https://github.com/SagoDev/Fragua",
