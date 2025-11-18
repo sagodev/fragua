b341676d1da1e9d436afadcd3f76141e00fba29d
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-11-18 20:04:47 -0300

    Update basic.py

--- fragua/environments/basic.py
+++ fragua/environments/basic.py
@@ -21,16 +21,12 @@ class BasicEnvironment(Environment):
     def __init__(self, name: str, fg_reg: bool = False):
         super().__init__(name, env_type="basic", fg_reg=fg_reg)
 
-        # Automatically build basic setup
         logger.debug("Creating BasicEnvironment '%s'...", self.name)
 
-        # 1. Warehouse
         warehouse = self.create_warehouse(f"{name}_warehouse")
 
-        # 2. Warehouse Manager
         self.create_manager(f"{name}_manager", warehouse)
 
-        # 3. Agents
         self.create_extractor(f"{name}_etr")
         self.create_transformer(f"{name}_tfr")
         self.create_loader(f"{name}_ldr")
