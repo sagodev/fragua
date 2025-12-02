f9ec5086efbcfe39f7113b5cd2965122627ca800
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-12-02 00:54:34 -0300

    Update registry.py

--- fragua/core/registry.py
+++ fragua/core/registry.py
@@ -1,15 +1,17 @@
 """Base class for all registries of an environment in Fragua."""
 
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 
 class Registry:
     """Configuration class for all registries types of an environment."""
 
-    def __init__(self, name: str, entries: Dict[str, Dict[str, Any]]) -> None:
+    def __init__(
+        self, name: str, entries: Optional[Dict[str, Dict[str, Any]]] = None
+    ) -> None:
         """Initialize the registry."""
-        self.name = name
-        self.entries = entries
+        self.name: str = name
+        self._entries: Dict[str, Dict[str, Any]] = {} if entries is None else entries
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}')"
