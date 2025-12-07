99151d1a42d905cb12cbf3923f54f9060bde04b4
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-12-07 13:47:22 -0300

    Update section_registry.py

--- fragua/core/section_registry.py
+++ fragua/core/section_registry.py
@@ -1,18 +1,24 @@
 """Section Registry class."""
 
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Type
 
-from fragua.core.entry_section import EntrySection
+from fragua.core.component import FraguaComponent
 
 
 class SectionRegistry(ABC):
     """Section registry class."""
 
-    def __init__(self, section_name: str):
+    def __init__(
+        self,
+        section_name: str,
+        entries: Optional[Dict[str, Type[FraguaComponent]]] = None,
+    ):
         """Initialize section registry."""
         self.section_name = section_name
-        self._entries: Dict[str, EntrySection] = {}
+        self._entries: Dict[str, Type[FraguaComponent]] = (
+            {} if entries is None else entries
+        )
 
     def _exists(self, key: str) -> bool:
         """Return True if the entry exists."""
@@ -22,19 +28,19 @@ class SectionRegistry(ABC):
         """Return True if the entry does not exist."""
         return key not in self._entries
 
-    def create_entry(self, name: str, component: Any) -> bool:
+    def create_entry(self, name: str, component: Type[FraguaComponent]) -> bool:
         """Create an entry linked to a component."""
 
         if self._not_exists(name):
-            self._entries[name] = EntrySection(name, component)
+            self._entries[name] = component
             return True
         return False
 
-    def get_entry(self, name: str) -> Optional[EntrySection]:
+    def get_entry(self, name: str) -> Optional[Type[FraguaComponent]]:
         """Retrieve a specific entry."""
         return self._entries.get(name)
 
-    def get_entries(self) -> Dict[str, EntrySection]:
+    def get_entries(self) -> Dict[str, Type[FraguaComponent]]:
         """Retrieve all entries."""
         return self._entries
 
@@ -42,7 +48,6 @@ class SectionRegistry(ABC):
         """Rename an entry."""
         if self._exists(old_name) and self._not_exists(new_name):
             entry = self._entries.pop(old_name)
-            entry.name = new_name
             self._entries[new_name] = entry
             return True
         return False
