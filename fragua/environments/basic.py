af0ca10110cf13e7b18bc85934392366e18b1f06
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-12-10 16:56:02 -0300

    Update basic.py

--- fragua/environments/basic.py
+++ fragua/environments/basic.py
@@ -19,14 +19,14 @@ class BasicEnvironment(Environment):
     - 1 Loader
     """
 
-    def __init__(self, name: str, fg_reg: bool = False):
-        super().__init__(name, env_type="basic", fg_reg=fg_reg)
+    def __init__(self, env_name: str, fg_reg: bool = False):
+        super().__init__(env_name=env_name, env_type="basic", fg_reg=fg_reg)
 
         logger.debug("Creating BasicEnvironment '%s'...", self.name)
 
-        self.create_extractor(f"{name}_etr")
-        self.create_transformer(f"{name}_tfr")
-        self.create_loader(f"{name}_ldr")
+        self.create_extractor(f"{env_name}_etr")
+        self.create_transformer(f"{env_name}_tfr")
+        self.create_loader(f"{env_name}_ldr")
 
         logger.info(
             "Basic environment '%s' created successfully with warehouse, manager, and 3 agents.",
