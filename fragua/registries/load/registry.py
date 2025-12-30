97f24c51e8083772a2be48806fcc1b7cef04a6a3
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-12-30 03:12:00 -0300

    Update registries

--- fragua/registries/load/registry.py
+++ fragua/registries/load/registry.py
@@ -3,10 +3,12 @@
 from fragua.core.registry import FraguaRegistry
 from fragua.core.set import FraguaSet
 from fragua.sets.load.functions import LOAD_FUNCTIONS
+from fragua.sets.load.internal_functions import LOAD_INTERNAL_FUNCTIONS
 from fragua.utils.types.enums import ComponentType
 
 LOAD_SETS = {
     ComponentType.FUNCTION.value: LOAD_FUNCTIONS,
+    ComponentType.INTERNAL_FUNCTION.value: LOAD_INTERNAL_FUNCTIONS,
     ComponentType.AGENT.value: FraguaSet("load_agents", components={}),
 }
 
--- fragua/registries/transform/registry.py
+++ fragua/registries/transform/registry.py
@@ -3,10 +3,12 @@
 from fragua.core.registry import FraguaRegistry
 from fragua.core.set import FraguaSet
 from fragua.sets.transform.functions import TRANSFORM_FUNCTIONS
+from fragua.sets.transform.internal_functions import TRANSFORM_INTERNAL_FUNCTIONS
 from fragua.utils.types.enums import ComponentType
 
 TRANSFORM_SETS = {
     ComponentType.FUNCTION.value: TRANSFORM_FUNCTIONS,
+    ComponentType.INTERNAL_FUNCTION.value: TRANSFORM_INTERNAL_FUNCTIONS,
     ComponentType.AGENT.value: FraguaSet("transform_agents", components={}),
 }
 
