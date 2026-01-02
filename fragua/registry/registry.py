963d3f3f701c8e21321a5adee94d90ca9142b3bf
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-02 17:04:20 -0300

    Update registry to use fragua-sets package

--- fragua/registry/registry.py
+++ fragua/registry/registry.py
@@ -1,21 +1,25 @@
 """Fragua Instance Registry."""
 
 from typing import Any, Dict
+
+from fragua_sets import (
+    PIPELINE_EXTRACT_FUNCTIONS,
+    PIPELINE_LOAD_FUNCTIONS,
+    PIPELINE_TRANSFORM_FUNCTIONS,
+    INTERNAL_LOAD_FUNCTIONS,
+    INTERNAL_TRANSFORM_FUNCTIONS,
+)
+
 from fragua.core.registry import FraguaRegistry
 from fragua.core.set import FraguaSet
-from fragua.sets.extract.functions import EXTRACT_FUNCTIONS
-from fragua.sets.load.functions import LOAD_FUNCTIONS
-from fragua.sets.load.internal_functions import INTERNAL_LOAD_FUNCTIONS
-from fragua.sets.transform.functions import TRANSFORM_FUNCTIONS
-from fragua.sets.transform.internal_functions import INTERNAL_TRANSFORM_FUNCTIONS
 
 
 FRAGUA_SETS: Dict[str, FraguaSet[Any]] = {
     "agent": FraguaSet("agent", components={}),
-    "extract_functions": EXTRACT_FUNCTIONS,
-    "transform_functions": TRANSFORM_FUNCTIONS,
+    "extract_functions": PIPELINE_EXTRACT_FUNCTIONS,
+    "transform_functions": PIPELINE_TRANSFORM_FUNCTIONS,
     "internal_transform_functions": INTERNAL_TRANSFORM_FUNCTIONS,
-    "load_functions": LOAD_FUNCTIONS,
+    "load_functions": PIPELINE_LOAD_FUNCTIONS,
     "internal_load_functions": INTERNAL_LOAD_FUNCTIONS,
 }
 
