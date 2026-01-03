561f1e91b96852a2fd7b225e10a347848f2eb6f1
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-03 11:47:44 -0300

    Refactor add function to handle list of FraguaStep

--- fragua/core/pipeline.py
+++ fragua/core/pipeline.py
@@ -1,5 +1,6 @@
 """Pipeline class."""
 
+from typing import List
 from fragua.core.step import FraguaStep
 
 
@@ -10,12 +11,20 @@ class FraguaPipeline:
 
     def __init__(self, name: str) -> None:
         self.name = name
-        self._steps: list[FraguaStep] = []
+        self._steps: List[FraguaStep] = []
 
-    def add(self, step: FraguaStep) -> None:
-        """Add step to pipeline."""
-        self._steps.append(step)
+    def add(self, step_or_steps: FraguaStep | List[FraguaStep]) -> None:
+        """Add an step or an ordened sequence of steps to pipeline."""
+        if isinstance(step_or_steps, List):
+            for step in step_or_steps:
+                self._steps.append(step)
+        if isinstance(step_or_steps, FraguaStep):
+            self._steps.append(step_or_steps)
 
-    def steps(self) -> list[FraguaStep]:
+        raise ValueError(
+            f"{step_or_steps} most be a FraguaStep or a order list of FraguaSteps."
+        )
+
+    def steps(self) -> List[FraguaStep]:
         """Retrive steps list."""
         return list(self._steps)
