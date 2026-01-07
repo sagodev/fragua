abee57026597ed9b9c8436439af6c97286e58c01
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-07 00:32:50 -0300

    Update set.py

--- fragua/core/set.py
+++ fragua/core/set.py
@@ -19,6 +19,9 @@ class FraguaSet:
 
     A FraguaSet stores named callables and/or pipelines and
     provides simple registration and lookup utilities.
+
+    The set can be configured to expose or not its callables
+    as FraguaStepBuilder templates.
     """
 
     def __init__(
@@ -26,6 +29,7 @@ class FraguaSet:
         name: str,
         *,
         items: Optional[Dict[str, FraguaItem]] = None,
+        step_enabled: bool = True,
     ) -> None:
         """
         Initialize the set.
@@ -36,8 +40,12 @@ class FraguaSet:
             Identifier of the set within a registry.
         items:
             Optional preloaded functions or pipelines.
+        step_enabled:
+            Whether callables registered in this set should
+            automatically generate FraguaStepBuilder templates.
         """
         self.name = name
+        self.step_enabled = step_enabled
         self._items: Dict[str, FraguaItem] = items or {}
 
     def exists(self, name: str) -> bool:
@@ -62,12 +70,12 @@ class FraguaSet:
         return self._items.get(name)
 
     def get_pipeline(self, name: str) -> Optional[FraguaPipeline]:
-        """Retrive an FraguaPipeline item. If not exist return None."""
+        """Retrieve a FraguaPipeline item. If not exist return None."""
         item = self.get(name)
         return item if isinstance(item, FraguaPipeline) else None
 
     def get_function(self, name: str) -> Optional[Callable[..., pd.DataFrame | Any]]:
-        """Retrive an Callable item. If not exist return None."""
+        """Retrieve a callable item. If not exist return None."""
         item = self.get(name)
         return item if callable(item) else None
 
