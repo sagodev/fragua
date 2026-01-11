b33cbd138753bd3b347eb37c0bd64505249a4c03
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-11 12:27:24 -0300

    Create variable DataFrameLike

--- fragua/utils/helpers/__init__.py
+++ fragua/utils/helpers/__init__.py
@@ -1,13 +1,19 @@
 """Helpers module."""
 
+from typing import TypeVar
+
 from .generate_steps_sequence import generate_steps_sequence
 from .transform_function_schema import transform_function_schema
 from .from_box_to_doc import from_box_to_doc
 from .get_project_root import get_project_root
+
+
+DataFrameLike = TypeVar("DataFrameLike")
 
 __all__ = [
     "generate_steps_sequence",
     "transform_function_schema",
     "from_box_to_doc",
     "get_project_root",
+    "DataFrameLike",
 ]
