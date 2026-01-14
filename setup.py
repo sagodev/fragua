daab65669a78a46721cfeaacc78d3319827bb43f
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-13 21:57:29 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="fragua",
-    version="1.6.3",
+    version="1.7.0",
     author="SagoDev",
     description=(
         "Framework for modeling, orchestrating,"
